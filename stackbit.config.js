8557b489de27415e965d3a0a76ec7d0be5c87cdd
Elad Rosenheim <elad@stackbit.com>
2023-04-02 18:39:07 +0300

    Make importable; bump packages

--- stackbit.config.js
+++ stackbit.config.js
@@ -7,12 +7,23 @@ const config = {
   contentSources: [
     new ContentfulContentSource({
       spaceId: process.env.CONTENTFUL_SPACE_ID,
-      environment: process.env.CONTENTFUL_ENVIRONMENT,
+      environment: process.env.CONTENTFUL_ENVIRONMENT || 'master',
       previewToken: process.env.CONTENTFUL_PREVIEW_TOKEN,
       accessToken: process.env.CONTENTFUL_MANAGEMENT_TOKEN,
     }),
   ],
   modelExtensions: [{ name: 'page', type: 'page', urlPath: '/{slug}' }],
+  // Needed only for importing this repository via https://app.stackbit.com/import?mode=duplicate
+  import: {
+    type: 'contentful',
+    contentFile: 'contentful/export.json',
+    uploadAssets: true,
+    assetsDirectory: 'contentful',
+    spaceIdEnvVar: 'CONTENTFUL_SPACE_ID',
+    deliveryTokenEnvVar: 'CONTENTFUL_DELIVERY_TOKEN',
+    previewTokenEnvVar: 'CONTENTFUL_PREVIEW_TOKEN',
+    accessTokenEnvVar: 'CONTENTFUL_MANAGEMENT_TOKEN',
+  },
 };
 
 export default config;
