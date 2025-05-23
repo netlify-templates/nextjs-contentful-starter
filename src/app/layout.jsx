0662c38b86e85cb567330491ac51b43f0348e3d8
Scopeyin <166521414+Scopeyin@users.noreply.github.com>
2025-05-23 13:08:05 +0530

    Update layout.jsx

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,8 +1,28 @@
 import '../../styles/globals.css';
+import Script from 'next/script';
 
 export default async function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        {/* Google Analytics */}
+        <Script
+          strategy="afterInteractive"
+          src="https://www.googletagmanager.com/gtag/js?id=G-PYS90MHM9WC"
+        />
+        <Script
+          id="gtag-init"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-PYS90MHM9WC');
+            `,
+          }}
+        />
+      </head>
       <body>
         {children}
       </body>
